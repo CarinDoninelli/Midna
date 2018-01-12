4e05bdedd597947f48a3c5d9abcd9a2dda0cd9fe
reguxp <reguxp@gmail.com>
2018-01-12 16:35:00 -0600

    pokemon jdbc

--- Dex/src/main/kotlin/com/carin/doninelli/dex/factory/DexFactory.kt
+++ Dex/src/main/kotlin/com/carin/doninelli/dex/factory/DexFactory.kt
@@ -4,7 +4,8 @@ package com.carin.doninelli.dex.factory
 
 import com.carin.doninelli.dex.Dex
 import com.carin.doninelli.dex.impl.jdbc.JdbcDex
+import com.carin.doninelli.dex.impl.jdbc.SqliteConnectionPool
 
 class DexFactory {
-    fun createDex(): Dex = JdbcDex()
+    fun createDex(): Dex = JdbcDex(SqliteConnectionPool)
 }
--- Dex/src/main/kotlin/com/carin/doninelli/dex/impl/jdbc/JdbcDex.kt
+++ Dex/src/main/kotlin/com/carin/doninelli/dex/impl/jdbc/JdbcDex.kt
@@ -3,9 +3,7 @@ package com.carin.doninelli.dex.impl.jdbc
 import com.carin.doninelli.dex.Dex
 import com.carin.doninelli.dex.entities.Pokemon
 
-class JdbcDex : Dex {
-
-    private val connectionPool = SqliteConnectionPool
+class JdbcDex internal constructor(private val connectionPool: SqliteConnectionPool) : Dex {
 
     override fun searchPokemon(id: Int): Pokemon? = connectionPool.getConnection().use { connection ->
         DexDao(connection).getPokemonById(id)
